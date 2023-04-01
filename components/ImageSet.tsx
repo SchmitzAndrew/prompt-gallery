3b7b29e99fbf7c0d4d84dc51e7ec2e24952f6217
SchmitzAndrew <schmitzandrew03@gmail.com>
2023-04-01 02:34:57 -0700

    add linking icons

--- components/ImageSet.tsx
+++ components/ImageSet.tsx
@@ -12,29 +12,46 @@
   }
   ```
 */
-import Image from 'next/image'
+import Image from 'next/image';
 
 interface ImageSetProps {
     image: string;
+    prompt: string;
     author: string;
     link: string;
 }
 
-export default function ImageSet({image, author, link}: ImageSetProps) {
-    
+interface FilePaths {
+    [key: string]: string;
+
+}
+
+export default function ImageSet({image, prompt, author, link}: ImageSetProps) {
     const dalle_fp = `/images/dalle/${image}.png`;
     const stable_fp = `/images/stable/${image}.png`;
     const midjourney_fp = `/images/midjourney/${image}.png`;
-    const filepaths = [dalle_fp, stable_fp, midjourney_fp];
+    const filepaths = { "DALLE:2": dalle_fp, "Stable Diffusion": stable_fp, "Midjourney": midjourney_fp};
+
+    let link_icon_filepath = `/link_icons/link.svg`;
+    if (link.includes("twitter")) {
+        link_icon_filepath = `/link_icons/twitter.svg`;
+    }
+    else if (link.includes("github")) {
+        link_icon_filepath = `/link_icons/github.svg`;
+    }
+    else if (link.includes("linkedin")) {
+        link_icon_filepath = `/link_icons/linkedin.svg`;
+    }
+    
 
     return (
             <div className="mx-auto max-w-7xl px-6">
-                <div className="mx-auto mt-14 grid max-w-2xl grid-cols-1 gap-y-20 gap-x-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-                    {filepaths.map((fp) => (
-                        <article key={fp} className="flex flex-col items-start justify-between">
+                <div className="mx-auto mt-8 grid max-w-2xl grid-cols-1 gap-x-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+                    {Object.entries(filepaths).map(([key,value]: [string, string]) => (
+                        <article key={key} className="flex flex-col items-start justify-between">
                             <div className="relative w-full">
                                 <Image
-                                    src={fp}
+                                    src={value}
                                     alt=""
                                     width={500}
                                     height={500}
@@ -45,7 +62,25 @@ export default function ImageSet({image, author, link}: ImageSetProps) {
                         </article>
                     ))}
                 </div>
+                <p className="">{prompt}</p>
+                <p className="">{author}</p>
+                <a href={link} target="_blank">
+                    <Image
+                        src={link_icon_filepath}
+                        alt={link_icon_filepath}
+                        width={100}
+                        height={100}
+                    />
+                </a>
+                
+                
+
+                        
+
+                   
             </div>
+        
+
         
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,11 @@ export default function Index() {
   return (
     <div>
         <h1 className="text-3xl font-bold underline">
-          Hello world!
+          Prompt Gallery
         </h1>
-      
-      <ImageSet image='rocket' author = 'Andrew Schmitz' link = 'https://twitter.com/Big_Schmitz'/>
-      <ImageSet image='koala' author ='Justin Eldrige' link= 'https://eldridgejm.github.io/'/>
-      <ImageSet image='rocket' author='Nikolas Huebecker' link= 'https://twitter.com/nhuebecker?lang=en'/>
+      <ImageSet image='rocket' prompt= 'rocket launch, stary night, digital art' author = 'Andrew Schmitz' link = 'https://twitter.com/Big_Schmitz'/>
+      <ImageSet image='koala' prompt= 'seventeen parachuting koalas' author ='Justin Eldrige' link= 'https://eldridgejm.github.io/'/>
+      <ImageSet image='rocket' prompt= 'Realistic 3d render of a happy, furry and cute baby red fox smiling with big eyes looking straight at you, Pixar style, 32k, full body shot with a light purple background' author='Nikolas Huebecker' link= 'https://twitter.com/nhuebecker?lang=en'/>
     </div>
     
   )
