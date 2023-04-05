19533a26d0d68dcd21338da500e38b623be47aba
SchmitzAndrew <schmitzandrew03@gmail.com>
2023-04-05 01:09:21 -0700

    fix fox image key

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,10 +10,10 @@ export default function Index() {
           Prompt Gallery
         </h1>
         <h2 className="text-white font-semibold pt-4">A collection of prompts and associated images from DALLE 2, Stable Diffusion, and Midjourney.</h2>
-        
+
         <ImageSet image='rocket' prompt= 'rocket launch, stary night, digital art' author = 'Andrew Schmitz' link = 'https://twitter.com/Big_Schmitz'/>
         <ImageSet image='koala' prompt= 'seventeen parachuting koalas' author ='Justin Eldrige' link= 'https://eldridgejm.github.io/'/>
-        <ImageSet image='rocket' prompt= 'Realistic 3d render of a happy, furry and cute baby red fox smiling with big eyes looking straight at you, Pixar style, 32k, full body shot with a light purple background' author='Nikolas Huebecker' link= 'https://twitter.com/nhuebecker?lang=en'/>
+        <ImageSet image='fox' prompt= 'Realistic 3d render of a happy, furry and cute baby red fox smiling with big eyes looking straight at you, Pixar style, 32k, full body shot with a light purple background' author='Nikolas Huebecker' link= 'https://twitter.com/nhuebecker?lang=en'/>
       </div>
     </div>
     
