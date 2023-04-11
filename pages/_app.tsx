3ee29274af2481d084c17490cf924bcd0c99bb61
SchmitzAndrew <schmitzandrew03@gmail.com>
2023-04-11 01:27:08 -0700

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,29 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Layout from "../components/Layout";
+import Layout from "../components/Layout"
+import Script from "next/script"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+    return (
+        <>
+            <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`} />
+            <Script
+                id='google-analytics'
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `,
+                }}
+            />
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </>
+    );
 }
