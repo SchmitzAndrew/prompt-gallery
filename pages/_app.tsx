e0c8f95a44a33299c84b149b55bbdd2dddc2f99a
SchmitzAndrew <schmitzandrew03@gmail.com>
2023-04-11 16:45:29 -0700

    install dotenv and try to fix analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,27 +3,30 @@ import type { AppProps } from "next/app";
 import Layout from "../components/Layout"
 import Script from "next/script"
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function mApp({ Component, pageProps }: AppProps) {
     return (
         <>
-            <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`} />
+            
             <Script
-                id='google-analytics'
                 strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `
+                src= {`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+            />
+
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
                     gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
                     page_path: window.location.pathname,
                     });
-                `,
-                }}
-            />
+                `}
+            </Script>
+
             <Layout>
                 <Component {...pageProps} />
             </Layout>
+            
         </>
     );
 }
